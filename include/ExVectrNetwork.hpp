d554e8742568aa79328c9e765ad13b952c32ee9c
ChSt15 <chrissteffen98@gmail.com>
2023-05-26 15:51:36 +0200

    Added Net namespace

--- include/ExVectrNetwork.hpp
+++ include/ExVectrNetwork.hpp
@@ -1,11 +1,17 @@
 #ifndef EXVECTRNETWORK_H_
 #define EXVECTRNETWORK_H_
 
-
 namespace VCTR
-{} // namespace VCTR The EXVECTR Library
+{
 
+    /**
+     * @brief The ExVectrNetwork library
+     */
+    namespace Net
+    {
 
+    }
 
+} // namespace VCTR The EXVECTR Library
 
 #endif
