c379ee9a623cb0e2d5df19a2aec6507ae77442f5
ChSt15 <chrissteffen98@gmail.com>
2023-05-26 15:53:55 +0200

    Added transport and network classes.Must implement

---
+++ include/ExVectrNetwork/network.hpp
@@ -0,0 +1,30 @@
+#ifndef EXVECTRNETWORK_NETWORK_H_
+#define EXVECTRNETWORK_NETWORK_H_
+
+
+
+namespace VCTR
+{
+
+    namespace Net
+    {
+
+        /**
+         * @brief Network layer abstract class.
+         */
+        class Network {
+        private:
+
+
+        public:
+
+            
+
+
+        };
+
+    }
+
+}
+
+#endif
---
+++ include/ExVectrNetwork/transport.hpp
@@ -0,0 +1,30 @@
+#ifndef EXVECTRNETWORK_TRANSPORT_H_
+#define EXVECTRNETWORK_TRANSPORT_H_
+
+
+
+namespace VCTR
+{
+
+    namespace Net
+    {
+
+        /**
+         * @brief Transport layer abstract class.
+         */
+        class Transport {
+        private:
+
+
+        public:
+
+            
+
+
+        };
+
+    }
+
+}
+
+#endif
